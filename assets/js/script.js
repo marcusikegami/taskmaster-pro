80b5410daac00e7bcb693f35d749b547392d7fe1
Marcus Ikegami <hikegami.6@gmail.com>
2021-10-08 12:17:35 -0600

    added ability to drag and delete tasks using jQuery UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -176,6 +176,68 @@ $("#remove-tasks").on("click", function() {
 });
 
 // load tasks for the first time
+
+
+$('.list-group').sortable({
+  connectWith: $('.list-group'),
+  scroll: false,
+  tolerance: 'pointer',
+  helper: 'clone',
+  activate: function(event) {
+    console.log('activate', this);
+  },
+  deactivate: function(event) {
+    console.log('deactivate', this)
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+
+var tempArr = [];
+
+    $(this).children().each(function() {
+      var text = $(this)
+      .find('p')
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find('span')
+      .text()
+      .trim();
+
+      console.log(text, date);
+
+      
+    });
+    var arrName = $(this)
+    .attr('id')
+    .replace('list-', '');
+
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    console.log('hello', tempArr);
+  }
+});
+$('#trash').droppable({
+  accept: '.list-group-item',
+  tolerance: 'touch',
+  drop: function(event, ui) {
+    console.log('drop');
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+
+});
+
 loadTasks();
-
-
